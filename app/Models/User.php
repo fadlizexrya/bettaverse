253c374b5b5034ece0ae8700816553f5a7216e28
Muhammad Fadli Zakaria <fadizz356@gmail.com>
2026-05-07 10:52:24 +0700

    userphp

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,15 +2,15 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Database\Factories\UserFactory;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
-    /** @use HasFactory<UserFactory> */
+    /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
 
     /**
@@ -45,5 +45,15 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    /**
+     * Izin akses ke panel Filament
+     */
+    public function canAccessPanel(Panel $panel): bool
+    {
+        // Untuk tahap awal (development), kita izinkan semua user login.
+        // Kedepannya kamu bisa batasi, misalnya: return str_ends_with($this->email, '@admin.com');
+        return true;
     }
 }
